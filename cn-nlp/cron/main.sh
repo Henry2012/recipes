b096cced8a8f06028e47f1dec375bdcba489a2f2
qiqun <qiqun.h@everstring.net>
2014-07-01 16:02:16 +0800

    Unindex some codebase

--- cn-nlp/cron/main.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-cd /mnt/data/qiqun.h/crawler_output/crawler-2014
-java -jar /mnt/data/qiqun.h/crawler_output/crawler-2014/crawler-zzw-2014-v2.jar
-
-now=`date +"%Y-%-m-%-d"`
-python /mnt/data/qiqun.h/cn-nlp/cron/main.py $now
